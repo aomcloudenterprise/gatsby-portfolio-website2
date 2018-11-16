48f8560490c0f44357e2748525126e73fe5f1153
danklut <aomcloudenterprise@outlook.com>
2018-11-16 11:45:34 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`
+    `gatsby-transformer-remark,`
     `gatsby-plugin-netlify',
     {
       resolve: `gatsby-source-filesystem`,
