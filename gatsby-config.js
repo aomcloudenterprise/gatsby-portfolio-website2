4a9673870e5ced69ceeb26e838ee2af02d208b73
danklut <aomcloudenterprise@outlook.com>
2018-11-15 21:36:46 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-
-
 require('dotenv').config()
 
 module.exports = {
@@ -28,26 +26,21 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: process.env.DATO_API_TOKEN, 
-      
-      // Disable automatic reloading of content when some change occurs on DatoCMS:
-      disableLiveReload: false,
-        
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area:
+        apiToken: `YOUR_READONLY_API_TOKEN`,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+
         // Custom API base URL
-      apiUrl: 'https://site-api.datocms.com',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        apiUrl: 'https://site-api.datocms.com',
       },
     },
-    'gatsby-plugin-offline',
-   },
- ],
-}        
+  ]
+}  
+     
