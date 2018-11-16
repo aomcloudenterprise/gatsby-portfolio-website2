4a649db0042a3e01b9fa1766f46bf3606de7fd34
danklut <aomcloudenterprise@outlook.com>
2018-11-16 08:55:28 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-remark`
+    `gatsby-plugin-netlify',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -49,7 +48,8 @@ module.exports = {
 
         // Custom API base URL
         apiUrl: 'https://site-api.datocms.com',
-      },
+      }, 
+      {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
@@ -59,6 +59,25 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+      {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
       },
     },
     'gatsby-plugin-offline',
