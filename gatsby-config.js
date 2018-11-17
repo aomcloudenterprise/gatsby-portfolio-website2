0f327c6d5ef29597bff8171583a3c14fca690990
danklut <aomcloudenterprise@outlook.com>
2018-11-16 17:20:20 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,7 @@ module.exports = {
       options: {
         name: `data`,
         path: `'${__dirname}/src/data/',
-        ignore: ['**./\.*'], // ignore files starting with a dot
-        path: path.join(__dirname, `src`, `images`),
+        ignore: ['** /\.*'], 
       },
     },
     {
